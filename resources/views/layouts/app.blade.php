39e82129f3ec81d77605e3e712e99dc840198cc1
root <root@walter-server.walter.gonzalez.net>
2025-10-30 16:23:19 -0600

    fix: Import custom CSS in app.css and rebuild assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
         <!-- Styles -->
-        @vite(['resources/css/app.css', 'resources/css/custom.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
